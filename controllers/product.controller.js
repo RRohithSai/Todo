af4deb1549f2ea85d6ef4094b70c7a25220b274a
RRohithSai <rohitsai.r@dhatvibs.com>
2025-10-23 12:18:30 +0530

    versel commited

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -31,6 +31,7 @@ const getProduct= async(req,res)=>{
 
 
 const createProduct= async (req,res) =>{
+    
              try{
             const product=await Product.create(req.body);
             res.status(200).json(product);   
